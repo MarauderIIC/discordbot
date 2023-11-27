ae8c204bc70cfd9968862abe36e287dbee376dc1
MarauderIIC <1060259+MarauderIIC@users.noreply.github.com>
2023-11-26 22:07:12 -0500

    fix serial oo

--- serialbot.py
+++ serialbot.py
@@ -1,90 +1,146 @@
 import concurrent.futures
+import logging
 import threading
 import asyncio
-import serial
-import sys
 import time
 from types import TracebackType
 from typing import Any, List, Optional
 
 import discord
 import discord.types
+import serial
+import serial.serialutil
 
 import discordbot
 
+_log = logging.getLogger()
+
 
 def flush(iface: serial.Serial) -> None:
-    print("Flushing serial...")
+    _log.info("Flushing serial...")
     while True:
         data = iface.read()
-        sys.stdout.write(data.decode())
+        _log.debug(data.decode())
         if not data:
             break
-    sys.stdout.write("\n")
-    print("- End flush")
+    _log.info("- End flush")
 
 
 class SerialChannel(discord.TextChannel):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         pass
 
-    async def send(self, content: Optional[str], **kwargs: None) -> None:  # type: ignore # This is a hack to make us minimally compatible.
+    async def send(self, content: Optional[str], **kwargs: None) -> None:  # type: ignore # Wrong return is a hack to make us minimally compatible.
         print(content)
         return None
 
 
+class SerialMember(discord.Member):
+    def __init__(self, discord_member: discord.Member) -> None:
+        self.discord_member = discord_member
+
+    @property
+    def mention(self) -> str:
+        return f"<@display={self.display_name} user={self.name} id={self.id}>"
+
+    def __getattr__(self, attr: str) -> Any:
+        return getattr(self.discord_member, attr)
+
+
 # TODO: Inherit from MarBot instead, and handle hardware_guild_id and hardware_user
 class SerialBot(discordbot.MarBot):
-    def __init__(self) -> None:
-        self.serial_user = "marauderiic"
+    def __init__(self, port, baud) -> None:
         discord.utils.setup_logging(
             handler=discord.utils.MISSING,
             formatter=discord.utils.MISSING,
             level=discord.utils.MISSING,
-            root=False,
+            root=True,
         )
-
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-        self.results: List[concurrent.futures.Future] = []  # type: ignore # There's only so much I can care about getting this typed
-
-        self.thread = threading.Thread(target=serial_thread, args=[self, self.loop])
 
         intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(intents=intents)
 
-    def handle_serial_message(self, serial_data: str) -> None:
-        user = discord.utils.find(lambda m: m.name == self.serial_user, self.users)
-        if user is None:
-            print(f"Can't find serial user {self.serial_user}")
+        self.serial_user = "marauderiic"
+        self.results: List[concurrent.futures.Future] = []  # type: ignore # There's only so much I can care about getting this typed
+
+        self.thread_done = False
+        self.thread = threading.Thread(target=thread_serial, args=[self, port, baud])
+
+    def thread_handle_serial_message(
+        self, serial_data: str, loop: asyncio.AbstractEventLoop
+    ) -> None:
+        """
+        Handle a serial command. Right now this means join the soundboard user's voice channel and play a sound.
+
+        :param serial_data: Raw serial string that represents a complete command
+        :param loop: The asyncio loop used to submit commands to the main MarBot task.
+        """
+        if not self.is_ready():
+            _log.warning(
+                f"Discord not connected; not ready to handle serial data: '{serial_data}'"
+            )
             return
+
+        if loop is None:
+            raise ValueError("No asyncio event loop")
+
+        user = None
+        for guild in self.guilds:
+            for member in guild.members:
+                if (
+                    member.name == self.serial_user
+                    and member.voice
+                    and member.voice.channel
+                ):
+                    user = member
+                    break
+
+        if user is None or user.voice is None or user.voice.channel is None:
+            _log.warning(
+                f"Can't find serial user {self.serial_user} in a voice channel in any server"
+            )
+            return
+
+        if not self.voice_client or (
+            self.voice_client and self.voice_client.channel != user.voice.channel
+        ):
+            self.results.append(
+                asyncio.run_coroutine_threadsafe(
+                    self.handle_join_user(user, SerialChannel(), []), loop
+                )
+            )
+            # TODO: Surely there's a callback for this.
+            timeout = time.time() + 5
+            while time.time() < timeout:
+                if self.voice_client and self.voice_client.is_connected():
+                    break
+                time.sleep(0.1)
+            else:
+                _log.warning(
+                    "Voice connection did not complete in timeout, try again when the bot joins"
+                )
+                return
 
         serial_data = serial_data.strip()
 
-        # TODO: Add a special join-me action handling here
-        # self.results.append(
-        #     asyncio.run_coroutine_threadsafe(
-        #         client.play_iface("serial-user", None, [data]), loop
-        #     )
-        # )
-
         self.results.append(
             asyncio.run_coroutine_threadsafe(
-                self.handle_play(user, SerialChannel(), [serial_data]), self.loop
+                self.handle_play(SerialMember(user), SerialChannel(), [serial_data]),
+                loop,
             )
         )
 
         # Prune outdated coroutines and their results.
         if len(self.results) > 5:
-            for result in self.results[:5]:  # Cancel the first 5
+            for result in self.results[:5]:  # Cancel the first 5 - an arbitrary number
                 result.cancel()
-            self.results = self.results[
-                5:
-            ]  # Continue waiting for everything after the first 5
+            # Continue waiting for everything else
+            self.results = self.results[5:]
 
     async def setup_hook(self) -> None:
         await super().setup_hook()
+        _log.debug("Starting serial thread")
         self.thread.start()
 
     def __enter__(self) -> "SerialBot":
@@ -100,52 +156,57 @@ class SerialBot(discordbot.MarBot):
         self.serial_bot_stop()
 
     def serial_bot_start(self) -> None:
-        discordbot.main(self, self.loop)
+        discordbot.main(self, loop)
 
     def serial_bot_stop(self) -> None:
-        global done
-        done = True
+        self.thread_done = True
         self.thread.join()
-        self.loop.run_until_complete(self.loop.shutdown_asyncgens())
-        self.loop.close()
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
 
 
 done = False  # Setting this flag causes the main_serial thread to stop.
 
 
-def serial_thread(client: SerialBot) -> None:
+def thread_serial(client: SerialBot, port: str, baud: int = 115200) -> None:
     """
     Poll for lines in the serial data
     """
-    global done
+    global loop # Putting this into SerialBot causes things to break... maybe the variable name overlaps with something?
 
-    print("Serial waiting for discord bot to come online")
-    while not done and not client.is_ready():
+    _log.info("Serial waiting for discord bot to come online")
+    # TODO: Use on_ready instead.
+    while not client.thread_done and not client.is_ready():
         time.sleep(0.1)
 
-    print("Starting serial")
     try:
-        with serial.Serial(port="COM3", baudrate=115200, timeout=1) as iface:
-            flush(iface)
-            data = ""
-            while not done:
-                # TODO: Handle serial.serialutil.SerialException here for when the soundboard is unplugged and plugged back in
-                rx_data = iface.readline().decode()
-                data += rx_data
-
-                if (
-                    done
-                ):  # Handle any flag changes that happened while we were reading data
-                    break  # type: ignore # This is reachable because 'done' is volatile.
-
-                if not data:
-                    continue
-
-                print("serial rx:", data.strip())
-
-                if "\n" in data:
-                    client.handle_serial_message(data)
+        while not client.thread_done:
+            try:
+                _log.info("Connecting to serial...")
+                with serial.Serial(port=port, baudrate=baud, timeout=1) as iface:
+                    flush(iface)
                     data = ""
+                    _log.info("Serial ready!")
+                    while not client.thread_done:
+                        # TODO: Handle serial.serialutil.SerialException here for when the soundboard is unplugged and plugged back in
+                        rx_data = iface.readline().decode()
+                        data += rx_data
+
+                        # Handle any flag changes that happened while we were reading data
+                        if client.thread_done:
+                            break   # type: ignore # This is reachable because thread_done is volatile
+
+                        if not data:
+                            continue
+
+                        _log.debug("serial rx:", data.strip())
+
+                        if "\n" in data:
+                            client.thread_handle_serial_message(data, loop)
+                            data = ""
+            except serial.serialutil.SerialException:
+                _log.warning("Can't talk to serial device. Retrying...")
+                time.sleep(5)
 
     except KeyboardInterrupt:
         pass
@@ -155,5 +216,10 @@ def serial_thread(client: SerialBot) -> None:
 
 
 if __name__ == "__main__":
-    with SerialBot() as client:
+    global loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    with SerialBot(port="COM3", baud=115200) as client:
         pass
